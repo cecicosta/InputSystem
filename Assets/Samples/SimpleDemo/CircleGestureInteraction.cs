7160e5ba1e313ca4e1c84abe022c721defe3c6ad
Cecilia <cecilia.costa@king.com>
2023-01-07 11:12:08 +0100

    Preparing skeleton for implementing custom interaction

---
+++ Assets/Samples/SimpleDemo/CircleGestureInteraction.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Editor;
+
+
+namespace Samples.SimpleDemo
+{
+#if UNITY_EDITOR
+    [UnityEditor.InitializeOnLoad]
+#endif
+[DisplayName("CircleGesture")]
+public class CircleGestureInteraction : IInputInteraction
+{
+    static CircleGestureInteraction()
+    {
+        InputSystem.RegisterInteraction<CircleGestureInteraction>();
+    }
+    public void Process(ref InputInteractionContext context)
+    {
+        var position = context.ReadValue<Vector2>();
+        Debug.Log("Mouse Position: " + position);
+    }
+
+    public void Reset()
+    {
+    }
+}
+
+#if UNITY_EDITOR
+/// <summary>
+/// UI that is displayed when editing <see cref="CircleGestureInteraction"/> in the editor.
+/// </summary>
+internal class CircleGestureInteractionEditor : InputParameterEditor<CircleGestureInteraction>
+{
+    protected override void OnEnable()
+    {
+    }
+
+    public override void OnGUI()
+    {
+    }
+
+}
+#endif
+}
